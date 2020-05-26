89971f5eeed7cf0f9e3084d4df07ae57029343fc
zyx <z4yx@users.noreply.github.com>
2020-05-26 15:43:33 +0800

    typo

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Install Denpendencies:
+# Install Dependencies:
 #   pip install requests socketIO-client
 # Or
 #   pip install -r requirements.txt
